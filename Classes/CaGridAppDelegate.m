30a9a93b3613ee3b13e7321abfffec5eafafa67f
rokickik <rokickik>
2009-08-16 17:46:30 +0000

    fixed some formatting
    SVN-Revision: 9

--- Classes/CaGridAppDelegate.m
+++ Classes/CaGridAppDelegate.m
@@ -1,7 +1,7 @@
 //
 //  CaGridAppDelegate.m
 //  CaGrid
-//
+// 
 //  Created by Konrad Rokicki on 6/24/09.
 //  Copyright __MyCompanyName__ 2009. All rights reserved.
 //
@@ -17,7 +17,7 @@
 @synthesize favoritesController;
 
 - (void)applicationDidFinishLaunching:(UIApplication *)application {
-    	
+	
     // Add the tab bar controller's current view as a subview of the window
 	[favoritesController loadFavorites];
 	
@@ -28,17 +28,17 @@
 	navigationController.tabBarItem = [[UITabBarItem alloc] initWithTabBarSystemItem:UITabBarSystemItemSearch tag:0];	
 	NSMutableArray *controllers = [NSMutableArray arrayWithArray:tabBarController.viewControllers];
 	tabBarController.viewControllers = [controllers arrayByAddingObject:navigationController];
-
+	
 	// add main view
     [window addSubview:tabBarController.view];
-
+	
 	// release objects
 	[searchController release];
 	[navigationController release];
 }
 
 - (void)tabBarController:(UITabBarController *)tabBarController didSelectViewController:(UIViewController *)viewController {
-
+	
 	// always show the root controller when switching to a tab
 	if ([viewController isKindOfClass:[UINavigationController class]]) {
 		UINavigationController* nav = (UINavigationController *)viewController;
@@ -53,10 +53,10 @@
 
 
 /*
-// Optional UITabBarControllerDelegate method
-- (void)tabBarController:(UITabBarController *)tabBarController didEndCustomizingViewControllers:(NSArray *)viewControllers changed:(BOOL)changed {
-}
-*/
+ // Optional UITabBarControllerDelegate method
+ - (void)tabBarController:(UITabBarController *)tabBarController didEndCustomizingViewControllers:(NSArray *)viewControllers changed:(BOOL)changed {
+ }
+ */
 
 
 - (void)dealloc {
