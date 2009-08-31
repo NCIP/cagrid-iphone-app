e0427e718c089b3ef2417894ee3e74d333457676
rokickik <rokickik>
2009-08-31 19:32:47 +0000

    updated javadocs
    SVN-Revision: 28

--- software/gss/src/gov/nih/nci/gss/api/Cab2bQuery.java
+++ software/gss/src/gov/nih/nci/gss/api/Cab2bQuery.java
@@ -11,9 +11,10 @@ import org.apache.log4j.Logger;
 
 /**
  * A background query which runs against the caB2B REST API. Specify the parameters
- * and kick off the query, and then check isDone() to see when it's completed.
- * Once isDone() is returning true, there will either be an error which is
- * accessible with getException() or a result accessible with getResultJson().
+ * and run this object in a thread. It will call the queryComplete() in the
+ * specified QueryService when it has completed. At that point isDone() will be 
+ * returning true, and there will either be an error which is accessible with 
+ * getException() or a result accessible with getResultJson().
  * 
  * @author <a href="mailto:rokickik@mail.nih.gov">Konrad Rokicki</a>
  */
@@ -31,12 +32,19 @@ public class Cab2bQuery implements Runnable, Serializable {
 
     private Exception exception;
     
-    
+    /**
+     * Create a new query, ready to be run.
+     * @param params the parameters to use
+     * @param service the QueryService to notify when the query is done
+     */
     public Cab2bQuery(Cab2bQueryParams params, QueryService service) {
         this.params = params;
         this.queryService = service;
     }
 
+    /**
+     * Implements Runnable.
+     */
     public void run() {
 
         try {
@@ -70,18 +78,35 @@ public class Cab2bQuery implements Runnable, Serializable {
         }
     }
 
+    /**
+     * Return the parameters for this query.
+     * @return
+     */
     public Cab2bQueryParams getQueryParams() {
         return params;
     }
 
+    /**
+     * Has the query been run and completed?
+     * @return
+     */
     public synchronized boolean isDone() {
         return isDone;
     }
 
+    /**
+     * Returns the resulting JSON. 
+     * Note: this may contain an application-specific error.
+     * @return
+     */
     public synchronized String getResultJson() {
         return resultJson;
     }
 
+    /**
+     * Returns the exception which occurred during query processing, if any.
+     * @return
+     */
     public synchronized Exception getException() {
         return exception;
     }
