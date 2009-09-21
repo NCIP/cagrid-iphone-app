102f7004260f152f5ac4463c6a6f227db06fba58
rokickik <rokickik>
2009-09-21 00:36:01 +0000

    updated primary keys
    SVN-Revision: 54

--- software/gss/sql/data_service_group.sql
+++ software/gss/sql/data_service_group.sql
@@ -3,8 +3,8 @@ INSERT INTO DATA_SERVICE_GROUP (ID,NAME,CAB2B_NAME,DATA_PRIMARY_KEY)
     VALUES (1,'microarray','Microarray Data','Public Experiment Identifier');
 
 INSERT INTO DATA_SERVICE_GROUP (ID,NAME,CAB2B_NAME,DATA_PRIMARY_KEY) 
-    VALUES (2,'biospecimen','Biospecimen Data','Public Experiment Identifier');
+    VALUES (2,'biospecimen','Biospecimen Data','Barcode');
 
 INSERT INTO DATA_SERVICE_GROUP (ID,NAME,CAB2B_NAME,DATA_PRIMARY_KEY) 
-    VALUES (3,'imaging','Imaging Data','Public Experiment Identifier');
+    VALUES (3,'imaging','Imaging Data','Image Study Instance UID');
     
