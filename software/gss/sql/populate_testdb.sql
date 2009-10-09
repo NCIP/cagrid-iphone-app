1b52ebbd23b1a4ca5188d0d4b0d32ec65f91b2bc
rokickik <rokickik>
2009-10-09 19:55:52 +0000

    added cab2b services
    SVN-Revision: 70

--- software/gss/sql/populate_testdb.sql
+++ software/gss/sql/populate_testdb.sql
@@ -2,4 +2,6 @@ source gss_schema.sql
 source data_service_group.sql
 source simple_names.sql
 source import_data_from_portal.sql
-source test_data/add_cab2b_service.sql
+update grid_service set url = concat(url,"/");
+source import_data_from_cab2b.sql
+#source test_data/add_cab2b_service.sql
