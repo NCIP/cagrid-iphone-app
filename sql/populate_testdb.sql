c51edacb04d69af18759746069e84a7a1041126b
rokickik <rokickik>
2009-10-17 21:56:13 +0000

    regenerated for 3.1.1
    SVN-Revision: 91

--- sql/populate_testdb.sql
+++ sql/populate_testdb.sql
@@ -4,4 +4,4 @@ source simple_names.sql
 source import_data_from_portal.sql
 update grid_service set url = concat(url,"/");
 source import_data_from_cab2b.sql
-#source test_data/add_cab2b_service.sql
+
