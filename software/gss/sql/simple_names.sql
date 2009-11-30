383164364c48046ed1012a527d7cdec9f5b20750
rokickik <rokickik>
2009-11-30 20:07:03 +0000

    added host patterns
    SVN-Revision: 139

--- software/gss/sql/simple_names.sql
+++ software/gss/sql/simple_names.sql
@@ -21,4 +21,10 @@ INSERT INTO SIMPLE_NAME (TYPE,PATTERN,SIMPLE_NAME) VALUES ("ServiceName","Creden
 INSERT INTO SIMPLE_NAME (TYPE,PATTERN,SIMPLE_NAME) VALUES ("ServiceName","FederatedQueryProcessor","Federated Query Processor ");
 
 INSERT INTO SIMPLE_NAME (TYPE,PATTERN,SIMPLE_NAME) VALUES ("HostName","NCI(\\s)?CB(IIT)?","NCI Center for Biomedical Informatics and Information Technology");
-INSERT INTO SIMPLE_NAME (TYPE,PATTERN,SIMPLE_NAME) VALUES ("HostName","National Cancer Institute Center for Bioinformatics - NCICB","NCI Center for Biomedical Informatics and Information Technology");
+INSERT INTO SIMPLE_NAME (TYPE,PATTERN,SIMPLE_NAME) VALUES ("HostName",".*?-\s?NCICB","NCI Center for Biomedical Informatics and Information Technology");
+INSERT INTO SIMPLE_NAME (TYPE,PATTERN,SIMPLE_NAME) VALUES ("HostName",".*?-\s?WUSTL","Washington University in St.Louis");
+INSERT INTO SIMPLE_NAME (TYPE,PATTERN,SIMPLE_NAME) VALUES ("HostName",".*?-\s?NCL","Nanotechnology Characterization Laboratory");
+INSERT INTO SIMPLE_NAME (TYPE,PATTERN,SIMPLE_NAME) VALUES ("HostName",".*?-\s?GTEM","GTEM");
+INSERT INTO SIMPLE_NAME (TYPE,PATTERN,SIMPLE_NAME) VALUES ("HostName",".*?-\s?Stanford","Stanford University Cancer Center");
+INSERT INTO SIMPLE_NAME (TYPE,PATTERN,SIMPLE_NAME) VALUES ("HostName","The Broad Institute.+","The Broad Institute");
+INSERT INTO SIMPLE_NAME (TYPE,PATTERN,SIMPLE_NAME) VALUES ("HostName","Center for Biomedical Informatics and Information Technology","NCI Center for Biomedical Informatics and Information Technology");
