866c165aec62ebfb329c925ed372b1e57ca2930f
piepenbringc <piepenbringc>
2009-12-04 17:45:47 +0000

    SVN-Revision: 143

--- software/gss/src/gov/nih/nci/gss/util/Constants.java
+++ software/gss/src/gov/nih/nci/gss/util/Constants.java
@@ -3,7 +3,7 @@ package gov.nih.nci.gss.util;
 public class Constants {
 
 	/** default intervalInMinutes*/
-	  public static final int DEFAULT_DISCOVERY_INTERVAL_IN_MINS = 60;
+	  public static final int DEFAULT_DISCOVERY_INTERVAL_IN_MINS = 720;
 
 	  public static final int MAX_SERVICE_DESC_LENGTH = 1024;
 }
