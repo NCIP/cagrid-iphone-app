3362ce29134e410c223e94d277dfcc06ce2f8a8f
rokickik <rokickik>
2009-12-14 16:40:37 +0000

    formatting
    SVN-Revision: 146

--- caGridApp/Classes/DownloadManager.m
+++ caGridApp/Classes/DownloadManager.m
@@ -7,6 +7,7 @@
 
 #import "DownloadManager.h"
 
+
 @interface Download : NSObject {
 	@private NSURL *url;
 	@private NSMutableData *receivedData;
@@ -17,6 +18,7 @@
 @property (nonatomic, retain) id delegate;
 
 @end
+
 
 @implementation Download
 @synthesize url;
@@ -31,12 +33,15 @@
 #pragma mark Object Methods
 
 - (id) init {
+	
 	if (self = [super init]) {
-        connectionRequestMap = CFDictionaryCreateMutable(kCFAllocatorDefault, 0, 
-                                                         &kCFTypeDictionaryKeyCallBacks, 
-                                                         &kCFTypeDictionaryValueCallBacks);
+        connectionRequestMap = CFDictionaryCreateMutable(
+								 kCFAllocatorDefault, 0, 
+								 &kCFTypeDictionaryKeyCallBacks, 
+								 &kCFTypeDictionaryValueCallBacks);
    	}
 	return self;
+	
 }
 
 #pragma mark -
@@ -49,7 +54,8 @@
     @try {
         NSMutableURLRequest *req = [NSMutableURLRequest requestWithURL:url];
         [req setHTTPMethod:@"GET"];
-        NSConnection *conn = [[NSURLConnection alloc] initWithRequest:req delegate:self];
+        NSConnection *conn = [[NSURLConnection alloc] 
+							  initWithRequest:req delegate:self];
         
         if (conn) {
             Download *dl = [[[Download alloc] init] autorelease];
@@ -58,89 +64,116 @@
             dl.delegate = delegateObj;
             
             @synchronized(self) {
-                [UIApplication sharedApplication].networkActivityIndicatorVisible = YES;
+                [UIApplication sharedApplication].
+					networkActivityIndicatorVisible = YES;
                 CFDictionaryAddValue(connectionRequestMap, conn, dl);
             }
             return;
         }
         
-        [dict setObject:@"Could not establish connection." forKey:NSLocalizedDescriptionKey];
+        [dict setObject:@"Could not establish connection." 
+				 forKey:NSLocalizedDescriptionKey];
     }
     @catch (NSException *exception) {
         NSLog(@"Error creating connection: %@",exception);
-        [dict setObject:@"Could not create network connection." forKey:NSLocalizedDescriptionKey];
+        [dict setObject:@"Could not create network connection." 
+				 forKey:NSLocalizedDescriptionKey];
     }
         
-    NSError *error = [NSError errorWithDomain:NSCocoaErrorDomain code:NSFileReadUnknownError userInfo:dict];
+    NSError *error = [NSError errorWithDomain:NSCocoaErrorDomain 
+										 code:NSFileReadUnknownError 
+									 userInfo:dict];
     if ([delegateObj respondsToSelector:@selector(download:failedWithError:)]) {
         [delegateObj download:url failedWithError:error];
     }
     else {
-        NSLog(@"WARNING: %@ does not respond to download:failedWithError:",[delegateObj class]);
+        NSLog(@"WARNING: %@ does not respond to download:failedWithError:",
+			  [delegateObj class]);
     }
+	
 }
 
 
 #pragma mark -
 #pragma mark NSURLConnection Delegate Methods
 
--(void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response {
-	Download *dl = (Download *)CFDictionaryGetValue(connectionRequestMap, connection);
+-(void)connection:(NSURLConnection *)connection 
+		didReceiveResponse:(NSURLResponse *)response {
+	
+	Download *dl = (Download *)CFDictionaryGetValue(connectionRequestMap, 
+													connection);
 	if (dl.receivedData != nil) {
         [dl.receivedData setLength:0];
     }
     else {
-        NSLog(@"WARNING: DownloadManager got response from unknown connection: %@",dl.url);
+        NSLog(@"WARNING: DownloadManager got response from unknown url: %@",dl.url);
     }
+	
 }
 
 
--(void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data {
-	Download *dl = (Download *)CFDictionaryGetValue(connectionRequestMap, connection);
+-(void)connection:(NSURLConnection *)connection 
+		didReceiveData:(NSData *)data {
+	
+	Download *dl = (Download *)CFDictionaryGetValue(connectionRequestMap, 
+													connection);
 	if (dl.receivedData != nil) {
         [dl.receivedData appendData:data];
     }
     else {
-        NSLog(@"WARNING: DownloadManager got response from unknown connection: %@",dl.url);
+        NSLog(@"WARNING: DownloadManager got response from unknown url: %@",dl.url);
     }
+	
 }
 
 
--(void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error {
-	Download *dl = (Download *)CFDictionaryGetValue(connectionRequestMap, connection);
+-(void)connection:(NSURLConnection *)connection 
+		didFailWithError:(NSError *)error {
+	
+	Download *dl = (Download *)CFDictionaryGetValue(connectionRequestMap, 
+													connection);
     
     if ([dl.delegate respondsToSelector:@selector(download:failedWithError:)]) {
         [dl.delegate download:dl.url failedWithError:error];
     }
     else {
-        NSLog(@"WARNING: %@ does not respond to download:failedWithError:",[dl.delegate class]);
+        NSLog(@"WARNING: %@ does not respond to download:failedWithError:",
+			  [dl.delegate class]);
     }
     
 	[connection release];
     
     @synchronized(self) {
 	    CFDictionaryRemoveValue(connectionRequestMap, connection);
-        [UIApplication sharedApplication].networkActivityIndicatorVisible = CFDictionaryGetCount(connectionRequestMap) > 0;
+        [UIApplication sharedApplication].networkActivityIndicatorVisible = 
+			CFDictionaryGetCount(connectionRequestMap) > 0;
     }
+	
 }
 
 
 -(void)connectionDidFinishLoading:(NSURLConnection *)connection {
-	Download *dl = (Download *)CFDictionaryGetValue(connectionRequestMap, connection);
+	
+	Download *dl = (Download *)CFDictionaryGetValue(connectionRequestMap, 
+													connection);
     
-    if ([dl.delegate respondsToSelector:@selector(download:completedWithData:)]) {
+    if ([dl.delegate respondsToSelector:
+			@selector(download:completedWithData:)]) {
         [dl.delegate download:dl.url completedWithData:dl.receivedData];
     }
     else {
-        NSLog(@"WARNING: %@ does not respond to download:completedWithData:",[dl.delegate class]);
+        NSLog(@"WARNING: %@ does not respond to download:completedWithData:",
+			  [dl.delegate class]);
     }
     
 	[connection release];
     
     @synchronized(self) {
 	    CFDictionaryRemoveValue(connectionRequestMap, connection);
-        [UIApplication sharedApplication].networkActivityIndicatorVisible = CFDictionaryGetCount(connectionRequestMap) > 0;
+        [UIApplication sharedApplication].networkActivityIndicatorVisible = 
+			CFDictionaryGetCount(connectionRequestMap) > 0;
     }
+	
 }
 
 @end
