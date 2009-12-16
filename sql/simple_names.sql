c34709e6d8532b4c2a9f668b8e38958a17d41d1b
rokickik <rokickik>
2009-12-16 07:03:31 +0000

    better rules for grid service name transformations
    SVN-Revision: 151

--- sql/simple_names.sql
+++ sql/simple_names.sql
@@ -1,30 +1,15 @@
 
 TRUNCATE TABLE SIMPLE_NAME;
 
-INSERT INTO SIMPLE_NAME (TYPE,PATTERN,SIMPLE_NAME) VALUES ("ServiceName","CaDSR.*?","caDSR");
-INSERT INTO SIMPLE_NAME (TYPE,PATTERN,SIMPLE_NAME) VALUES ("ServiceName","EVS.*?","Enterprise Vocabulary Service");
-INSERT INTO SIMPLE_NAME (TYPE,PATTERN,SIMPLE_NAME) VALUES ("ServiceName",".*?CaArray.*?","caArray");
-INSERT INTO SIMPLE_NAME (TYPE,PATTERN,SIMPLE_NAME) VALUES ("ServiceName","caTissue_Core.*?","caTissue Core");
-INSERT INTO SIMPLE_NAME (TYPE,PATTERN,SIMPLE_NAME) VALUES ("ServiceName","caTissue_Suite.*?","caTissue Suite");
-INSERT INTO SIMPLE_NAME (TYPE,PATTERN,SIMPLE_NAME) VALUES ("ServiceName",".*?CaTissueCore.*?","caTissue Core");
-INSERT INTO SIMPLE_NAME (TYPE,PATTERN,SIMPLE_NAME) VALUES ("ServiceName",".*?CaTissueSuite.*?","caTissue Suite");
-INSERT INTO SIMPLE_NAME (TYPE,PATTERN,SIMPLE_NAME) VALUES ("ServiceName","NCIA.*?","NCIA");
-INSERT INTO SIMPLE_NAME (TYPE,PATTERN,SIMPLE_NAME) VALUES ("ServiceName","caTissue.*?","caTissue");
-INSERT INTO SIMPLE_NAME (TYPE,PATTERN,SIMPLE_NAME) VALUES ("ServiceName","ChromosomalSegmentOverlapAcrossSources","CSOAS");
-INSERT INTO SIMPLE_NAME (TYPE,PATTERN,SIMPLE_NAME) VALUES ("ServiceName","OpenClinicaCRF","OpenClinical CRF");
-INSERT INTO SIMPLE_NAME (TYPE,PATTERN,SIMPLE_NAME) VALUES ("ServiceName","WorkflowFactoryService","Workflow Factory");
-INSERT INTO SIMPLE_NAME (TYPE,PATTERN,SIMPLE_NAME) VALUES ("ServiceName","TavernaWorkflowService","Taverna");
-INSERT INTO SIMPLE_NAME (TYPE,PATTERN,SIMPLE_NAME) VALUES ("ServiceName","AuthenticationService","Authentication");
-INSERT INTO SIMPLE_NAME (TYPE,PATTERN,SIMPLE_NAME) VALUES ("ServiceName","GlobalModelExchange","GME");
-INSERT INTO SIMPLE_NAME (TYPE,PATTERN,SIMPLE_NAME) VALUES ("ServiceName","GridGrouper","Grid Grouper");
-INSERT INTO SIMPLE_NAME (TYPE,PATTERN,SIMPLE_NAME) VALUES ("ServiceName","CredentialDelegationService","Credential Delegation");
-INSERT INTO SIMPLE_NAME (TYPE,PATTERN,SIMPLE_NAME) VALUES ("ServiceName","FederatedQueryProcessor","Federated Query Processor ");
+INSERT INTO SIMPLE_NAME (TYPE,PATTERN,SIMPLE_NAME) VALUES ("ServiceName","Svc_v\\d+_\\d+$","");
+INSERT INTO SIMPLE_NAME (TYPE,PATTERN,SIMPLE_NAME) VALUES ("ServiceName","(caBIO)\\d+","\\1");
+INSERT INTO SIMPLE_NAME (TYPE,PATTERN,SIMPLE_NAME) VALUES ("ServiceName","camod","caMOD");
 
 INSERT INTO SIMPLE_NAME (TYPE,PATTERN,SIMPLE_NAME) VALUES ("HostName","NCI(\\s)?CB(IIT)?","NCI Center for Biomedical Informatics and Information Technology");
-INSERT INTO SIMPLE_NAME (TYPE,PATTERN,SIMPLE_NAME) VALUES ("HostName",".*?-\s?NCICB","NCI Center for Biomedical Informatics and Information Technology");
-INSERT INTO SIMPLE_NAME (TYPE,PATTERN,SIMPLE_NAME) VALUES ("HostName",".*?-\s?WUSTL","Washington University in St.Louis");
-INSERT INTO SIMPLE_NAME (TYPE,PATTERN,SIMPLE_NAME) VALUES ("HostName",".*?-\s?NCL","Nanotechnology Characterization Laboratory");
-INSERT INTO SIMPLE_NAME (TYPE,PATTERN,SIMPLE_NAME) VALUES ("HostName",".*?-\s?GTEM","GTEM");
-INSERT INTO SIMPLE_NAME (TYPE,PATTERN,SIMPLE_NAME) VALUES ("HostName",".*?-\s?Stanford","Stanford University Cancer Center");
+INSERT INTO SIMPLE_NAME (TYPE,PATTERN,SIMPLE_NAME) VALUES ("HostName",".*?-\\s?NCICB","NCI Center for Biomedical Informatics and Information Technology");
+INSERT INTO SIMPLE_NAME (TYPE,PATTERN,SIMPLE_NAME) VALUES ("HostName",".*?-\\s?WUSTL","Washington University in St.Louis");
+INSERT INTO SIMPLE_NAME (TYPE,PATTERN,SIMPLE_NAME) VALUES ("HostName",".*?-\\s?NCL","Nanotechnology Characterization Laboratory");
+INSERT INTO SIMPLE_NAME (TYPE,PATTERN,SIMPLE_NAME) VALUES ("HostName",".*?-\\s?GTEM","GTEM");
+INSERT INTO SIMPLE_NAME (TYPE,PATTERN,SIMPLE_NAME) VALUES ("HostName",".*?-\\s?Stanford","Stanford University Cancer Center");
 INSERT INTO SIMPLE_NAME (TYPE,PATTERN,SIMPLE_NAME) VALUES ("HostName","The Broad Institute.+","The Broad Institute");
 INSERT INTO SIMPLE_NAME (TYPE,PATTERN,SIMPLE_NAME) VALUES ("HostName","Center for Biomedical Informatics and Information Technology","NCI Center for Biomedical Informatics and Information Technology");
