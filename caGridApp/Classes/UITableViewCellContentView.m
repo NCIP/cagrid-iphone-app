609e7f26dc7744a60e57fa0b03f77013a9eaeb9c
rokickik <rokickik>
2010-02-03 02:49:48 +0000

    added fix for SDK 3.2
    SVN-Revision: 185

---
+++ caGridApp/Classes/UITableViewCellContentView.m
@@ -0,0 +1,17 @@
+//
+//  UITableViewCellContentView.m
+//  CaGrid
+//
+
+#include "UITableViewCellContentView.h"
+@implementation UITableViewCellContentView
+
++ (id)alloc {
+    return [UIView alloc];
+}
+
++ (id)allocWithZone:(NSZone *)zone {
+    return [UIView allocWithZone:zone];
+}
+
+@end
