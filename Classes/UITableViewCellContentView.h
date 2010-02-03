98a596397bb495c3b57354d818bd904219f07158
rokickik <rokickik>
2010-02-03 02:49:48 +0000

    added fix for SDK 3.2
    SVN-Revision: 185

---
+++ Classes/UITableViewCellContentView.h
@@ -0,0 +1,10 @@
+//
+//  UITableViewCellContentView.h
+//  Added to fix issue with SDK 3.2:
+// http://stackoverflow.com/questions/2152321/weird-uitableviewcell-loading-exception
+//
+
+#import <UIKit/UIKit.h>
+@interface UITableViewCellContentView : UIView {
+}
+@end
