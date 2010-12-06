0011d63ca542c7ea1356c067998c95ddab17ac21
rokickik <rokickik>
2010-12-06 16:20:25 +0000

    clean up
    SVN-Revision: 330

--- software/gss/src/gov/nih/nci/gss/grid/GridServiceVerifier.java
+++ software/gss/src/gov/nih/nci/gss/grid/GridServiceVerifier.java
@@ -12,7 +12,6 @@ import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpHost;
 import org.apache.http.HttpRequest;
 import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.log4j.Logger;
